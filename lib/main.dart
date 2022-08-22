fdcafc3322c20546bc8ed5e73560d350936c85e3
epillef1311 <103550993+epillef1311@users.noreply.github.com>
2022-08-22 18:08:34 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:tragicomic/src/auth/sign_up_screen.dart';
 import 'src/auth/sign_in_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const TragiComic());
 }
 
@@ -15,21 +16,18 @@ class TragiComic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+        ),
+        debugShowCheckedModeBanner: false,
 
-      //Definindo rotas no APP
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SignInScreen(),
-        '/signUp':(context) => SignUpScreen(),
-        '/inicialScreen':(context) => InicialScreen(),
-
-      }
-      
-    );
+        //Definindo rotas no APP
+        initialRoute: '/',
+        routes: {
+          '/': (context) => SignInScreen(),
+          '/signUp': (context) => SignUpScreen(),
+          '/inicialScreen': (context) => const InicialScreen(),
+        });
   }
 }
