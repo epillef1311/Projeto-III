229da67824df52d335561b8ca569cc3dd29d36d7
epillef1311 <103550993+epillef1311@users.noreply.github.com>
2022-08-22 18:03:33 -0300

    Add files via upload

--- lib/inicial_screen.dart
+++ lib/inicial_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tragicomic/firebase_options.dart';
+import 'package:tragicomic/src/auth/sign_in_screen.dart';
 
 class InicialScreen extends StatelessWidget {
   const InicialScreen({Key? key}) : super(key: key);
@@ -6,9 +9,20 @@ class InicialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
-    return Scaffold (
-      backgroundColor: Colors.blueGrey,
-    );
-    
+    return Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return SignInScreen();
+              default:
+                return const Text('Loading...');
+            }
+          },
+        ));
   }
 }
--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,8 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tragicomic/src/auth/components/custom_text_field.dart';
 
-class SignInScreen extends StatelessWidget {
-  const SignInScreen({Key? key}) : super(key: key);
+class SignInScreen extends StatefulWidget {
+  SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  late final TextEditingController _name;
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+  late final TextEditingController _telefone;
+
+  @override
+  void initState() {
+    _name = TextEditingController();
+    _email = TextEditingController();
+    _password = TextEditingController();
+    _telefone = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _name.dispose();
+    _email.dispose();
+    _password.dispose();
+    _telefone.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,145 +50,163 @@ class SignInScreen extends StatelessWidget {
           child: Column(
             children: [
               Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-
-                    //NOME DO APP
-                    Text.rich(
-                      TextSpan(
-                        style: TextStyle(
-                          fontSize: 40,
-                        ),
-                        children: [
-                          TextSpan(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  //NOME DO APP
+                  Text.rich(TextSpan(
+                      style: TextStyle(
+                        fontSize: 40,
+                      ),
+                      children: [
+                        TextSpan(
                             text: 'Tragicomic',
                             style: TextStyle(
                               color: Colors.black,
                               fontWeight: FontWeight.bold,
-                            )
-                          ),
-                        ]
-                      )
-                    ),
-                    
-                  ],
-                )
-              ),
-      //O container crescerá ao máximo para acomodoar seus filhos,
-      //o  EXPANDED acima irá crescer o máximo que puder sem atraplhar o container;
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 30,
-                vertical: 30,
-              ),
-      
-              decoration: const BoxDecoration(
-                color: Colors.white),
-      
+                            )),
+                      ])),
+                ],
+              )),
+              //O container crescerá ao máximo para acomodoar seus filhos,
+              //o  EXPANDED acima irá crescer o máximo que puder sem atraplhar o container;
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 30,
+                ),
+
+                decoration: const BoxDecoration(color: Colors.white),
+
                 //Faz a parte branca da tela com cantos arredondados
                 //borderRadius: BorderRadius.vertical(top: Radius.circular(45))),
-      
-              child: Column(
-                // Estica todos os botões
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  //Nome do usuário
-                  const CustomTextField(
-                    icon: Icons.person,
-                    label: 'Usuário',
-                  ),
-      
-                  // Senha
-                  const CustomTextField(
-                    icon: Icons.lock,
-                    label: 'Senha',
-                    isSecret: true,
-                  ),
-                  
-                  // BOTÂO PARA ENTRAR
-                  SizedBox (
-                    height: 45,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),)
-                      ),
-                      onPressed:(){
-                        Navigator.of(context).pushNamed('/inicialScreen');
-                      }, 
-                      child: const 
-                      Text('Acessar', style: TextStyle(fontSize: 18,
-                      ),)
+
+                child: Column(
+                  // Estica todos os botões
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    //Nome do usuário
+                    TextField(
+                      controller: _email,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(hintText: 'Email'),
                     ),
-                   ),
-                  
-                  //Esqueceu senha?
+
+                    // Senha
+                    TextField(
+                      controller: _password,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: const InputDecoration(hintText: 'Senha'),
+                    ),
+
+                    // BOTÂO PARA ENTRAR
+                    SizedBox(
+                      height: 45,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          )),
+                          onPressed: () async {
+                            final email = _email.text;
+                            final password = _password.text;
+                            try {
+                              final UserCredential = await FirebaseAuth.instance
+                                  .signInWithEmailAndPassword(
+                                      email: email, password: password);
+                              print(UserCredential);
+                            } on FirebaseAuthException catch (e) {
+                              if (e.code == 'user-not-found') {
+                                print('user not found');
+                              } else if (e.code == 'wrong-password') {
+                                print('wrong password');
+                              }
+                            }
+                            Navigator.of(context).pushNamed('/inicialScreen');
+                          },
+                          child: const Text(
+                            'Acessar',
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          )),
+                    ),
+
+                    //Esqueceu senha?
                     Align(
                       alignment: Alignment.centerLeft,
                       //Padding: Espaçamento entre o texto 'esqueci sinha' e os botões.
                       child: Padding(
                         padding: const EdgeInsets.only(top: 5),
                         child: TextButton(
-                          onPressed: (){},
-                          child: const Text ('Esqueceu a senha?', 
-                          style: TextStyle(
-                            color: Colors.green,
+                          onPressed: () {},
+                          child: const Text(
+                            'Esqueceu a senha?',
+                            style: TextStyle(
+                              color: Colors.green,
+                            ),
                           ),
                         ),
-                    ),
                       ),
-                  ),
-      
-                // DIVISOR COM 'ou'
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Row (
-                      children:const[
-                        Expanded (
-                          child: Divider(
-                            color: Colors.blueGrey,
-                            thickness: 2,
-                            ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 15),
-                          child: Text('ou'),
-                        ),
-                        Expanded(
-                          child: Divider(
-                            color: Colors.blueGrey,
-                            thickness: 2,
-                            ),
-                        ),
-                      ],
                     ),
-                  ),
-                
-                //Botão: CRIAR CONTA
-                SizedBox(
-                  height: 45,
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18),),
-                        side: const BorderSide(
-                        width: 2,
-                        color: Colors.blueGrey,
-                      )
+
+                    // DIVISOR COM 'ou'
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Row(
+                        children: const [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.blueGrey,
+                              thickness: 2,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            child: Text('ou'),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.blueGrey,
+                              thickness: 2,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    onPressed: (){
-                      Navigator.of(context).pushNamed('/signUp');
-                    }, 
-                    child: const Text('Criar Conta',
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),),),),
-                ],
+
+                    //Botão: CRIAR CONTA
+                    SizedBox(
+                      height: 45,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                            side: const BorderSide(
+                              width: 2,
+                              color: Colors.blueGrey,
+                            )),
+                        onPressed: () {
+                          Navigator.of(context).pushNamed('/signUp');
+                        },
+                        child: const Text(
+                          'Criar Conta',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-             ),
-          ],
-             ),
+            ],
+          ),
         ),
       ),
     );
